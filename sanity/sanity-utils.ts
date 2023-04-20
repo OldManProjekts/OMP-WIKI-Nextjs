6ca136867fde78de940d1d2f036958a5fa16a5b0
OldManProjekts <100838258+OldManProjekts@users.noreply.github.com>
2023-04-20 12:23:13 +0200

    Project Pages Updated

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -18,5 +18,26 @@ export async function getProjects(): Promise<Project[]>{
             url,
             content,
         }`
+    );
+}
+
+export async function getProject(slug: string): Promise<Project> {
+    const client = createClient({
+        projectId: "vb34kuwz",
+        dataset: "production",
+        apiVersion: '2023-04-12',
+    });
+
+    return client.fetch(
+        groq`*[_type == "project" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content,
+        }`,
+        {slug: slug}
     )
 }
